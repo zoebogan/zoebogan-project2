1ba174a622fca392434676d89ea69cd0eb989ad5
zoebogan <zoe.bogan@gwu.edu>
2024-11-17 21:44:05 -0500

    outline the required functions

---
+++ Client.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    
+
+    public Client(String hostname, int port) throws IOException {
+       
+    }
+
+    public Socket getSocket() {
+        return sock;
+    }
+
+    public void handshake() {
+       
+    }
+
+    public String request(String factorizeNumber) throws IOException {
+        
+    }
+
+    public void disconnect() {
+        
+    }
+}
---
+++ Server.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+import java.time.LocalDateTime;
+import java.util.concurrent.*;
+import java.util.*;
+
+public class Server {
+   
+
+    public Server(int port) throws IOException {
+        
+    }
+
+    public void serve(int activeClients) {
+       
+    }
+
+    private class ClientHandler extends Thread {
+        
+
+        public ClientHandler(Socket sock) {
+           
+        }
+
+        public void run() {
+        
+        }
+    }
+    
+    public ArrayList<LocalDateTime> getConnectedTimes() {
+    
+    }
+   
+    public String factorize(int number) {
+  
+    }
+
+    public void disconnect() {
+ 
+    }
+}
