a84b87e18a784cc9ca2e0b698fb9363b78c4feb7
zoebogan <zoe.bogan@gwu.edu>
2024-11-17 22:17:38 -0500

    completion of Client.java with passing test cases

--- Client.java
+++ Client.java
@@ -3,10 +3,14 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-    
+    private Socket sock;
+    private PrintWriter outgoing;
+    private BufferedReader incoming;
 
     public Client(String hostname, int port) throws IOException {
-       
+        this.sock = new Socket(hostname, port);
+        this.incoming = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        this.outgoing = new PrintWriter(sock.getOutputStream(), true);
     }
 
     public Socket getSocket() {
@@ -14,14 +18,24 @@ public class Client {
     }
 
     public void handshake() {
-       
+        outgoing.println("12345");
+        outgoing.flush();
     }
 
     public String request(String factorizeNumber) throws IOException {
-        
+        outgoing.println(factorizeNumber);
+        String response = incoming.readLine();
+        return response;
     }
 
     public void disconnect() {
-        
+        try {
+            incoming.close();
+            outgoing.close();
+            sock.close();
+        } catch (IOException e) {
+            System.err.println("Error occured during disconnecting " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
